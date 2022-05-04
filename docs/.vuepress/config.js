2c91f4fc6d80cb7dd289d907303b13f6fc685a6f
shadow <657075528@qq.com>
2022-05-04 09:06:23 +0800

    fix: 更改路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,61 @@
 module.exports = {
   title: "shadow",
   description: "Just playing around",
-  base: "/blog/",
+  base: "/",
   themeConfig: {
     sidebar: [
       {
         title: "element相关",
-        path: "/blog/element/element",
+        path: "/blob/element/element",
         collapsable: false,
-        children: [{ title: "穿透更改element", path: "/blog/element/element" }],
+        children: [{ title: "穿透更改element", path: "/blob/element/element" }],
       },
       {
         title: "babel相关",
-        path: "/blog/babel",
+        path: "/blob/babel",
         collapsable: false,
         children: [
           {
             title: "babel-plugin-remove-development",
-            path: "/blog/babel/babel-plugin-remove-development",
+            path: "/blob/babel/babel-plugin-remove-development",
           },
         ],
       },
       {
         title: "vue源码相关",
-        path: "/blog/vue",
+        path: "/blob/vue",
         collapsable: false,
         children: [
           {
             title: "vue2computed源码解析",
-            path: "/blog/vue/vue2-computed",
+            path: "/blob/vue/vue2-computed",
           },
           {
             title: "vue2watch源码解析",
-            path: "/blog/vue/vue2-watch",
+            path: "/blob/vue/vue2-watch",
           },
           {
             title: "vue2$set源码解析",
-            path: "/blog/vue/vue2-$set",
+            path: "/blob/vue/vue2-$set",
           },
         ],
       },
       {
         title: "日常记录",
-        path: "/blog/daily",
+        path: "/blob/daily",
         collapsable: false,
         children: [
           {
             title: "控制重复请求,并返回同段时间内的请求结果",
-            path: "/blog/daily/control-request",
+            path: "/blob/daily/control-request",
           },
           {
             title: "团队代码规范",
-            path: "/blog/daily/team-rule",
+            path: "/blob/daily/team-rule",
           },
           {
             title: "团队代码规范",
-            path: "/blog/daily/vue+docker+jenkins",
+            path: "/blob/daily/vue+docker+jenkins",
           },
         ],
       },
