5910396045c4b4035db344a8f93b18b4233db355
shadow <657075528@qq.com>
2022-05-04 09:47:18 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:kaelstart/blob.git master:main
+git push https://github.com/kaelstart/blog.git master:gh-pages
 
 cd -
